bb859ce6ad90a5de0b4c757761c8c9d79b8e1bcf
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-03-31 15:57:18 +0200

    - Make CompositePrimitiveController call superclass XML constructor

---
+++ libcbf/composite_primitive_controller.cc
@@ -0,0 +1,19 @@
+#include <cbf/composite_primitive_controller.h>
+
+#ifdef CBF_HAVE_XSD
+	#include <schemas.hxx>
+#endif
+
+namespace CBF {
+
+#ifdef CBF_HAVE_XSD
+
+CompositePrimitiveController::CompositePrimitiveController(const CompositePrimitiveControllerType &xml_instance) :
+	PrimitiveController(xml_instance)
+{
+	
+}
+
+} // namespace
+
+#endif
