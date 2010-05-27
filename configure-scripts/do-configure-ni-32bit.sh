77214bc37abf02379defc0bb9ea108c6f74f6b25
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-05-27 13:30:33 +0200

    adapted ni configure script to find eigen2 installation

--- configure-scripts/do-configure-ni-32bit.sh
+++ configure-scripts/do-configure-ni-32bit.sh
@@ -2,4 +2,4 @@
 
 # This script is only useful for members of the AG neuroinformatics at uni bielefeld
 
-./waf configure --with-xercesc-dir=/vol/xcf --with-xsd-include-dir=$HOME/src/build/xsd-3.2.0+dep/install/include/ --with-xsd=/homes/fschmidt/src/build/xsd-3.2.0+dep/install/bin/xsd  --with-kdl-dir=/vol/orocos --with-eigen2-include-dir=/vol/numerik/ --with-kdl-chain-viz-dir=/homes/fschmidt/local --prefix=/vol/nirobots/ --with-pa10-dir=/vol/nirobots --with-spacenavi-dir=/vol/nirobots/  $*
+./waf configure --with-xercesc-dir=/vol/xcf --with-xsd-include-dir=$HOME/src/build/xsd-3.2.0+dep/install/include/ --with-xsd=/homes/fschmidt/src/build/xsd-3.2.0+dep/install/bin/xsd  --with-kdl-dir=/vol/orocos --with-eigen2-include-dir=/vol/numerik/eigen2 --with-kdl-chain-viz-dir=/homes/fschmidt/local --prefix=/vol/nirobots/ --with-pa10-dir=/vol/nirobots --with-spacenavi-dir=/vol/nirobots/  $*
