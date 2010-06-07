b44626599448e5ecd333ed29e755529bd6a6d5dc
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-06-07 15:46:50 +0200

    masked resource compiles

--- libcbf/cbf/masking_resource.h
+++ libcbf/cbf/masking_resource.h
@@ -5,6 +5,9 @@
 #include <cbf/resource.h>
 #include <cbf/plugin_decl_macros.h>
 
+#include <vector>
+#include <stdexcept>
+
 CBF_PLUGIN_PREAMBLE(MaskingResource)
 
 namespace CBF {
@@ -19,24 +22,47 @@ namespace CBF {
 struct MaskingResource : public Resource {
 	CBF_PLUGIN_DECL_METHODS(MaskingResource)
 
-	MaskingResource(ResourcePtr masked_resource = ResourcePtr()) :
-		m_Resource(masked_resource)
+	MaskingResource(ResourcePtr masked_resource, std::vector<unsigned int> indexes)
 	{
+		set_resource_and_indexes(masked_resource, indexes);
+	}
 
+	virtual void set_resource_and_indexes(ResourcePtr masked_resource, std::vector<unsigned int> indexes) {
+		for (unsigned int i = 0; i < m_Indexes.size(); ++i)
+			if (m_Indexes[i] > m_Resource->dim()) 
+				throw std::runtime_error("Index out of bounds");
+
+		m_Result = FloatVector(m_Indexes.size());
+	}
+	
+	virtual void update() {
+		m_Resource->update();
+
+		for (unsigned int i = 0, len = m_Indexes.size(); i < len; ++i)
+			m_Result[i] = m_Resource->get()[m_Indexes[i]];
 	}
 
 	virtual void set(const FloatVector &arg) { }
 
-	virtual const FloatVector &get() { return m_Result; }
+	virtual const FloatVector &get() { 
+		return m_Result; 
+	}
 
-	virtual void add(const FloatVector &arg) { }
+	virtual void add(const FloatVector &arg) { 
+		FloatVector tmp = ublas::zero_vector<Float>(m_Resource->get().size());
+
+		for (unsigned int i = 0, len = m_Indexes.size(); i < len; ++i)
+			tmp[m_Indexes[i]] = arg[i];
+
+		m_Resource->add(tmp);
+	}
 
 	virtual unsigned int dim() { return 0; }
 
 	protected:
 		ResourcePtr m_Resource;
-
 		FloatVector m_Result;
+		std::vector<unsigned int> m_Indexes;
 };
 
 } // namespace
