35cc957a6ad03d6be8cf04fc630ff4d526fc3e45
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-06-21 00:14:53 +0200

    minor fixes

--- configure-scripts/do-configure-home.sh
+++ configure-scripts/do-configure-home.sh
@@ -1,1 +1,1 @@
-./waf configure  --with-kdl-dir=/home/tapas/local/opt/KDL/ --prefix=/home/tapas/local --with-eigen2-include-dir=/usr/include/eigen2
+./waf configure  --with-kdl-dir=/home/tapas/local/opt/KDL/ --prefix=/home/tapas/local --with-eigen2-include-dir=/usr/include/eigen2 $*
