f797b5175d8eba6568513fd27b9a7d1b517e6bf1
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-07-19 16:13:08 +0200

    removed some waf crud

--- configure-scripts/do-configure-ni-32bit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is only useful for members of the AG neuroinformatics at uni bielefeld
-
-./waf configure --with-xercesc-dir=/vol/xcf --with-xsd-include-dir=$HOME/src/build/xsd-3.2.0+dep/install/include/ --with-xsd=/homes/fschmidt/src/build/xsd-3.2.0+dep/install/bin/xsd  --with-kdl-dir=/vol/orocos --with-eigen2-include-dir=/vol/numerik/eigen2 --with-kdl-chain-viz-dir=/homes/fschmidt/local --prefix=/vol/nirobots/ --with-pa10-dir=/vol/nirobots --with-spacenavi-dir=/vol/nirobots/  $*
--- configure-scripts/do-configure-ni-64bit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is useful only to members of the AG neuroinformatics at Bielefeld University
-
-./waf configure --with-xsd=/homes/fschmidt/local/opt/xsd-3.2.0-i686-linux-gnu/bin/xsd --with-xsd-include-dir=$HOME/local/opt/xsd-3.2.0-i686-linux-gnu/libxsd/ --with-kdl-dir=/vol/tim --with-eigen2-include-dir=/vol/numerik/ --with-kdl-chain-viz-dir=/vol/tim --prefix=/vol/nirobots/ $*
