a4ee96935ea02aa8c02d7260482cb5c99f8295aa
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-09-10 20:23:05 +0200

    made xcf reference client more flexible

--- tests/cbf_test_xcf_reference_client.cc
+++ tests/cbf_test_xcf_reference_client.cc
@@ -3,18 +3,49 @@
 #include <cbf/schemas.hxx>
 #include <cbf/debug_macros.h>
 
+#include <string>
 #include <iostream>
 #include <sstream>
+#include <unistd.h>
 
 using namespace CBF;
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		std::cout << "Usage: cbf_test_xcf_reference_client name dimension" << std::endl;
+		exit (EXIT_FAILURE);
+	}
+
 	CBF_DEBUG("creating remote server object")
-	XCF::RemoteServerPtr _remoteServer = XCF::RemoteServer::create("CBFReference");
- 		
+	XCF::RemoteServerPtr _remoteServer = XCF::RemoteServer::create(argv[1]);
+
+	std::stringstream str(argv[2]);
+	unsigned int dim;
+	str >> dim;
+
 	while(true) {
+		std::cout << "Enter " << dim << " values" << std::endl;
+
+		std::string temp;
+		std::cin >> temp;
+
+		std::cout << "Entered: " << temp << std::endl;
+		
+		std::stringstream vector_string;
+		std::stringstream user_input(temp);
+
+		vector_string << "[" << dim << "](";
+		for (unsigned int i = 0; i < dim; ++i) {
+			float tmp;
+			user_input >> tmp;
+			vector_string << tmp;
+			if (i != dim - 1) vector_string << ",";
+		}
+		vector_string << ")";
+
 		CBF_DEBUG("creating vector doc")
-		BoostVectorType v("[5](1,1,1,1,1)");
+		BoostVectorType v(vector_string.str());
+
 		std::ostringstream s;
 		Vector (s, v);
 
