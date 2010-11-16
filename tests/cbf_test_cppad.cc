cd3f2ef2ffb3a94046e7543f62b55e4591659c88
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-11-16 15:23:31 +0100

    made test 2 dim

--- tests/cbf_test_cppad.cc
+++ tests/cbf_test_cppad.cc
@@ -5,24 +5,29 @@
 int main() {
 	using CppAD::AD;
 
-	boost::numeric::ublas::vector<AD<double> > x(1);
+	boost::numeric::ublas::vector<AD<double> > x(2);
 	x[0] = 0;
+	x[1] = 1;
 	CppAD::Independent(x);
 
 	boost::numeric::ublas::vector<AD<double> > y(1);
-	y[0] = CppAD::sin(x[0]);
+	y[0] = CppAD::cos(x[0]+1.2) + 5.0 * CppAD::sin(x[1]);
 
 	CppAD::ADFun<double> f(x,y);
 
-	boost::numeric::ublas::vector<double> x_p(1);
+	boost::numeric::ublas::vector<double> x_p(2);
 	for (unsigned int i = 0; i < 100; ++i) {
 		x_p[0] = (i/100.0) * (2.0 * M_PI);
+		x_p[1] = 0;
 		std::cout << i << " " << x_p[0] << " " << f.Jacobian(x_p)[0] << " "  <<  std::endl;
 	}
 
-	CBF::CppADSensorTransformPtr s(new CBF::CppADSensorTransform(f, 1,1));
+	CBF::CppADSensorTransformPtr s(new CBF::CppADSensorTransform(f, 1, 2));
 
-	s->update(CBF::FloatVector(1));
+	CBF::FloatVector res(2);
+	res[0] = res[1] = 1;
+
+	s->update(res);
 
 	std::cout << s->result() << std::endl;
 	std::cout << s->task_jacobian() << std::endl;
