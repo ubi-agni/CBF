fe183ab832c4860abd2590f0c38910251d3cae01
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-11-25 15:25:59 +0100

    formatting to improve readability

--- libcbf/cbf/norm_transform.h
+++ libcbf/cbf/norm_transform.h
@@ -43,7 +43,12 @@ struct NormSensorTransform : public SensorTransform {
 		m_Transform->update(resource_value);
 		m_Result[0] = ublas::norm_2(m_Transform->result());
 
-		FloatVector res2 = (1.0/m_Result[0]) *  ublas::prod(ublas::trans(m_Transform->result()), m_Transform->task_jacobian());
+		FloatVector res2 = 
+			(1.0/m_Result[0]) 
+			* ublas::prod(
+				ublas::trans(m_Transform->result()), 
+				m_Transform->task_jacobian()
+			);
 
 		for (unsigned int i = 0; i < res2.size(); ++i) {
 			m_TaskJacobian(0,i) = res2(i);
