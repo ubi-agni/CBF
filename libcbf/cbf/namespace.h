e513cf4b6a47172c8b3e5017aacaca08ca87e77b
Florian Paul Schmidt <mista.tapas@gmx.net>
2010-12-18 11:38:11 +0100

    forgot to add files

---
+++ libcbf/cbf/namespace.h
@@ -0,0 +1,16 @@
+#ifndef CBF_NAMESPACE_HH
+#define CBF_NAMESPACE_HH
+
+#include <cbf/object.h>
+#include <boost/shared_ptr.hpp>
+#include <map>
+
+namespace CBF {
+	struct ObjectNamespace {
+		typedef std::map<std::string, ObjectPtr> map;
+	};
+
+	
+} // namespace
+
+#endif
---
+++ libcbf/namespace.cc
@@ -0,0 +1,2 @@
+#include <cbf/namespace.h>
+
