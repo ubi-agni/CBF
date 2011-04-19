a177c0d6c8f226c5e056de119281691854e1c832
Florian Paul Schmidt <mista.tapas@gmx.net>
2011-04-19 15:31:13 +0200

    almost finished the tree and chain viewer wrappers

--- libcbf/cbf/kdl_chain_view_transforms.h
+++ libcbf/cbf/kdl_chain_view_transforms.h
@@ -3,8 +3,10 @@
 
 #include <cbf/sensor_transform.h>
 #include <cbf/namespace.h>
+#include <cbf/kdl_transforms.h>
 
-#include <kdlchainview.h>
+#include <qkdlchainview.h>
+#include <qkdltreeview.h>
 
 namespace CBFSchema {
 	class KDLChainViewSensorTransform;
@@ -16,42 +18,69 @@ namespace CBF {
 struct KDLChainViewSensorTransform : public SensorTransform {
 	KDLChainViewSensorTransform(const CBFSchema::KDLChainViewSensorTransform &xml_instance, ObjectNamespacePtr object_namespace);
 
-	KDLChainViewSensorTransform(SensorTransformPtr operand) {
-
+	KDLChainViewSensorTransform(KDLChainPositionSensorTransformPtr operand) {
+		init(operand);
 	}
 
-	void init(SensorTransformPtr operand) {
-
+	void init(KDLChainPositionSensorTransformPtr operand) {
+		m_Operand = operand;
+		m_ChainView = boost::shared_ptr<KDLCV::QKDLChainView>(new KDLCV::QKDLChainView(operand->chain().get()));
+		m_ChainView->show();
 	}
 
 	virtual void update(const FloatVector &res) {
 		m_Operand->update(res);
+		std::copy(res.begin(), res.end(), m_ChainView->pose().begin());
+		m_ChainView->update();
 	}
 
-	SensorTransformPtr m_Operand;
+
+	virtual unsigned int task_dim() const { return m_Operand->task_dim(); }
+	virtual unsigned int resource_dim() const { return m_Operand->resource_dim(); }
+
+	virtual const FloatVector& result() const { return m_Operand->result(); }
+	virtual const FloatMatrix& task_jacobian() const { return m_Operand->task_jacobian(); }
+
+	KDLChainPositionSensorTransformPtr m_Operand;
+
+	boost::shared_ptr<KDLCV::QKDLChainView> m_ChainView;
 };
+
+
+
 
 
 struct KDLTreeViewSensorTransform : public SensorTransform {
 	KDLTreeViewSensorTransform(const CBFSchema::KDLTreeViewSensorTransform &xml_instance, ObjectNamespacePtr object_namespace);
 
-	KDLTreeViewSensorTransform(SensorTransformPtr operand) {
-
+	KDLTreeViewSensorTransform(KDLTreePositionSensorTransformPtr operand) {
+		init(operand);
 	}
 
-	void init(SensorTransformPtr operand) {
-
+	void init(KDLTreePositionSensorTransformPtr operand) {
+		m_Operand = operand;
+		m_TreeView = boost::shared_ptr<KDLCV::QKDLTreeView>(new KDLCV::QKDLTreeView(operand->tree().get()));
+		m_TreeView->show();
 	}
 
 	virtual void update(const FloatVector &res) {
 		m_Operand->update(res);
+		std::copy(res.begin(), res.end(), m_TreeView->pose().begin());
+		m_TreeView->update();
 	}
 
-	virtual const FloatVector& result() { return m_Operand->result(); }
-	virtual const FloatMatrix& task_jacobian() { return m_Operand->task_jacobian(); }
 
-	SensorTransformPtr m_Operand;
+	virtual unsigned int task_dim() const { return m_Operand->task_dim(); }
+	virtual unsigned int resource_dim() const { return m_Operand->resource_dim(); }
+
+	virtual const FloatVector& result() const { return m_Operand->result(); }
+	virtual const FloatMatrix& task_jacobian() const { return m_Operand->task_jacobian(); }
+
+	KDLTreePositionSensorTransformPtr m_Operand;
+
+	boost::shared_ptr<KDLCV::QKDLTreeView> m_TreeView;
 };
+
 
 } // namespace
 
--- libcbf/kdl_chain_view_transforms.cc
+++ libcbf/kdl_chain_view_transforms.cc
@@ -6,10 +6,28 @@ namespace CBF {
 #ifdef CBF_HAVE_XSD
 	KDLChainViewSensorTransform::KDLChainViewSensorTransform(const CBFSchema::KDLChainViewSensorTransform &xml_instance, ObjectNamespacePtr object_namespace) {
 
-		SensorTransformPtr s = XMLObjectFactory::instance()->create<SensorTransform>(xml_instance.Operand(), object_namespace);
+		KDLChainPositionSensorTransformPtr s = 
+			XMLObjectFactory::instance()->create<KDLChainPositionSensorTransform>(
+				xml_instance.Operand(), object_namespace
+			);
+
+		init(s);
 	}
 
-	XMLDerivedFactory<KDLChainViewSensorTransform, CBFSchema::KDLChainViewSensorTransform> x;
+	static XMLDerivedFactory<KDLChainViewSensorTransform, CBFSchema::KDLChainViewSensorTransform> x;
+
+	KDLTreeViewSensorTransform::KDLTreeViewSensorTransform(const CBFSchema::KDLTreeViewSensorTransform &xml_instance, ObjectNamespacePtr object_namespace) {
+
+		KDLTreePositionSensorTransformPtr s = 
+			XMLObjectFactory::instance()->create<KDLTreePositionSensorTransform>(
+				xml_instance.Operand(), object_namespace
+			);
+
+		init(s);
+	}
+
+	static XMLDerivedFactory<KDLTreeViewSensorTransform, CBFSchema::KDLTreeViewSensorTransform> x2;
+
 #endif
 
 } // namespace
